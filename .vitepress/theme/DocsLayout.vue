b90d499efe31b7bedf76b88f078f9f94e547cc23
nolhan <nolhan.dev@gmail.com>
2026-04-18 17:31:16 +0200

    fix: fixed logo theme in sidebar

--- .vitepress/theme/DocsLayout.vue
+++ .vitepress/theme/DocsLayout.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
-import { computed, onMounted } from 'vue'
-import { useData } from 'vitepress'
+import { onMounted } from 'vue'
 
 const { Layout } = DefaultTheme
-const { isDark } = useData()
-
-const imageLink = computed(() =>
-  isDark.value
-    ? 'https://r2.discordanalytics.xyz/images/brand/long_logo_dark.webp'
-    : 'https://r2.discordanalytics.xyz/images/brand/long_logo_light.webp',
-)
 
 onMounted(() => {
   const html = document.querySelector('html')
@@ -24,7 +16,16 @@ onMounted(() => {
 <template>
   <Layout>
     <template #nav-bar-title-before>
-      <img :src="imageLink" alt="Discord Analytics" class="logo" />
+      <img
+        src="https://r2.discordanalytics.xyz/images/brand/long_logo_dark.webp"
+        alt="Discord Analytics"
+        class="logo dark-only"
+      />
+      <img
+        src="https://r2.discordanalytics.xyz/images/brand/long_logo_light.webp"
+        alt="Discord Analytics"
+        class="logo light-only"
+      />
     </template>
   </Layout>
 </template>
